79d6cc77dd19c09cc778729652473afb9ddc12ad
Loïc Pernot <lac.pernot@gmail.com>
2024-11-01 12:07:52 +0100

    Implement C, R, and D operations
    U is in progress

--- Java/RecipEService/src/main/java/ch/unil/doplab/domain/ApplicationState.java
+++ Java/RecipEService/src/main/java/ch/unil/doplab/domain/ApplicationState.java
@@ -6,19 +6,25 @@ import ch.unil.doplab.MealPlan;
 import ch.unil.doplab.UserProfile;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.UUID;
+
+import java.util.*;
 
 @ApplicationScoped
 public class ApplicationState {
     private Map<UUID, UserProfile> userProfiles;
-
+    private Map<String, UUID> usernames;
+    // We use both a userProfiles and usernames list so requests can be made using both the UUID and the username
 
     @PostConstruct
     public void init() {
         userProfiles = new TreeMap<>();
+        usernames = new TreeMap<>();
     }
+
+    // In order : getters, add operations, remove operations
+    public UserProfile getUserProfile(UUID id) {return userProfiles.get(id);}
+
+    public Map<UUID, UserProfile> getAllUserProfiles() {return userProfiles;}
 
     public UserProfile addUserProfile(UserProfile userProfile) {
         if (userProfile.getUserId() != null) {
@@ -28,11 +34,47 @@ public class ApplicationState {
     }
 
     public UserProfile addUserProfile(UUID id, UserProfile userProfile) {
+        if (userProfile.getUsername() == null | userProfile.getUsername().isEmpty()) {
+            throw new IllegalArgumentException("Username cannot be null or empty.");
+        }
+        if (userProfiles.containsKey(id)) {
+            throw new IllegalArgumentException("User with ID " + id + " already exists.");
+        }
+        if (usernames.containsKey(userProfile.getUsername())) {
+            throw new IllegalArgumentException("Username " + userProfile.getUsername() + " already in use.");
+        }
         userProfiles.put(id, userProfile);
+        usernames.put(userProfile.getUsername(), id);
         return userProfile;
     }
 
-    public UserProfile getUserProfile(UUID id) {return userProfiles.get(id);}
+    public boolean setUserProfile(UUID id, UserProfile userProfile) {
+        // This function changes the data to whatever userProfile is saved at UUID id to whatever data
+        // is in userProfile
+        UserProfile oldUserProfile = userProfiles.get(id);
+        if (userProfile == null) {
+            return false;
+        }
+        // Translating the following condition : if the old & new user profiles have the same (non-null) username
+        // but their UUIDs are different (i.e. you are trying to set the same username for two different users
+        // then throw an exception
+        if (oldUserProfile.getUsername().equals(userProfile.getUsername())
+                && oldUserProfile.getUsername() != null
+                && !usernames.get(userProfile.getUsername()).equals(id)) {
+            throw new IllegalArgumentException("A user with username " + userProfile.getUsername() + " already exists.");
+        }
+        // TODO implement userProfile.updateUser() function
+        return true;
+    }
 
-    public Map<UUID, UserProfile> getAllUserProfiles() {return userProfiles;}
+    public void removeUserProfile(String username){
+        removeUserProfile(usernames.get(username));
+    }
+
+    public void removeUserProfile(UUID id) {
+        usernames.remove(userProfiles.get(id).getUsername());
+        userProfiles.remove(id);
+    }
+
+
 }
